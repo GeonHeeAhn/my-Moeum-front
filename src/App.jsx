ae4eeda083e96ae7d84b7ed2f39b91185b6f3d16
GeonHeeAhn <justamoment@ewhain.net>
2023-12-04 12:23:44 +0900

    chage baseUrl

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ import FriendPostList from './Pages/FriendPostList';
 
 //axios.defaults.baseURL = "http://localhost:5000";
 axios.defaults.baseURL =
-  'http://ec2-15-164-103-67.ap-northeast-2.compute.amazonaws.com:5000';
+  'https://api.moeum.site';
 
 function App() {
   return (
